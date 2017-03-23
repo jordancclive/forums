a852d017848fc16d811144e6f6b00088e7a841dd
jordancclive <jordancclive@gmail.com>
2017-03-23 15:46:22 -0400

    made a lot of progress

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-router.get('/', (req,res,next)=>{
-  res.render('index', { page: 'Home' });
+router.get('/', (req, res, next)=> {
+  db.models.Forums.findAll({ order: 'topic' })
+    .then( result => res.render('index', { forums: result }))
+    .catch(next);
+});
+
+router.get('/forum/:topic', (req, res, next)=>{
+  db.models.Forums.findOne({ where: { topic: req.params.topic }, include: [{ model: db.models.Users }]})
+    .then(result => res.render('forum', { forum: result }))
 })
 
-router.use('/forum', (req, res, next)=>{
-  res.render('forum', { page: 'Forums' });
+router.use('/profile', (req, res, next)=>{
+  res.render('profile', { page: 'Profile' });
 })
 
 module.exports = router;
